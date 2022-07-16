a2fe90864b95e2f8c3459f8fd72a98d1303f4122
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-07-16 08:29:32 -0400

    Add files via upload

---
+++ pset1.py
@@ -0,0 +1,21 @@
+# Dont touch
+name = input("what is your name?: ")
+print(f"welcome {name}")
+
+
+
+number = 300
+
+a = [1, 2, 3]
+
+# TODO 
+# add number to your list 
+
+
+# TODO 
+# Raise number to power 2 and store in b
+
+
+# TODO
+# append it to the list
+
